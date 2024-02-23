19ea44c33135262ef01b4546784352a2e56f0cd4
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-02-23 15:38:51 +0300

    SecurityConfig added

---
+++ in-memory/src/main/java/com/cengizhanyavuz/security/inmemory/security/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.cengizhanyavuz.security.inmemory.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user1 = User.builder()
+                .username("Cengizhan")
+                .password("1234")
+                .roles("USER")
+                .build();
+
+        UserDetails admin1 = User.builder()
+                .username("admin")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin1);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws  Exception {
+        security
+                .headers(x -> x.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(x -> x.requestMatchers("/public/**","auth/**").permitAll())
+                .authorizeHttpRequests(x -> x.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return  security.build();
+
+    }
+}
